e493ad3d22629be7ca926d3aa095c2cfee2d6c1d
Sagi Sarussi <sagis@sagis.dev>
2020-11-20 01:35:42 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+#Github Crawler
+
+##Description:
+Iterates over all [trending](https://github.com/trending) repositories and prints their information
+
+WIP
---
+++ main.py
@@ -0,0 +1,56 @@
+import requests as r
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+import json
+
+
+def get_url(url):
+    res = r.get(url)
+    if res.status_code != 200:
+        print("Got status code " + str(res.status_code) + " from: " + res.url)
+        exit(1)
+
+    return res
+
+
+def trending_repos(res):
+    props = {'names': list(), "owners": list(), "languages": list()}
+
+    doc = bs(res.text, "lxml")
+    # repos_box = doc.find("div", class_="Box")
+
+    for repo in doc.find_all("article", class_="Box-row"):
+        populate_names(repo, props)
+        populate_languages(repo, props)
+
+    print(json.dumps(props))
+
+    return pd.DataFrame(props)
+
+
+def name_printer(names, owners):
+    for i in range(len(names)):
+        print(owners[i] + "/" + names[i])
+
+
+def populate_names(elem, list_dict):
+    owner, _, name = elem.find("h1", class_="h3 lh-condensed").text.strip().split()
+    list_dict["owners"].append(owner)
+    list_dict["names"].append(name)
+
+
+def populate_languages(elem, list_dict):
+    if (lang := elem.find("span", itemprop="programmingLanguage")) is not None:
+        list_dict["languages"].append(lang.text)
+    else:
+        list_dict["languages"].append("N/A")
+
+
+# def trending_devs(URL):
+#
+
+if __name__ == '__main__':
+    URL = "https://github.com/trending"
+    df = trending_repos(get_url(URL))
+
+    print(df)
